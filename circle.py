b6556a105727805e09a234378821ebd1497a7fdf
Sarah Kirk <kirkse710@gmail.com>
2018-07-09 16:12:20 -0700

    adds simple class structure for circle

---
+++ circle.py
@@ -0,0 +1,13 @@
+class Circle():
+    pi = 3.14
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * Circle.pi
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+mycircle = Circle(3)
+print(mycircle.area())
