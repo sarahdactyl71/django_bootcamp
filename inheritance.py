36d3f894a51e8089e35d4c7ed2e3909c400f3f80
Sarah Kirk <kirkse710@gmail.com>
2018-07-09 16:21:08 -0700

    adds bark method to inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -15,9 +15,13 @@ class Dog(Animal):
         Animal.__init__(self)
         print("DOG CREATED")
 
+    def bark(self):
+        print("BORK BORK")
+
 mydog = Dog()
 mydog.who_am_i()
 mydog.eat()
+mydog.bark()
 
 myanimal = Animal()
 myanimal.who_am_i()
