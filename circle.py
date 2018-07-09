3fb1347358fa61d771bed20d0569bf35a9055915
Sarah Kirk <kirkse710@gmail.com>
2018-07-09 16:14:51 -0700

    adds circumference method to circle class

--- circle.py
+++ circle.py
@@ -6,8 +6,12 @@ class Circle():
     def area(self):
         return self.radius * self.radius * Circle.pi
 
+    def circumference(self):
+        return 2 * Circle.pi * self.radius
+
     def set_radius(self, new_radius):
         self.radius = new_radius
 
 mycircle = Circle(3)
 print(mycircle.area())
+print (mycircle.circumference())
